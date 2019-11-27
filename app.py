41986cf0f03e40c4b817fa1aaada4177ec74fbab
linzero <806307567@qq.com>
2019-11-27 09:30:04 +0800

    添加路由规则

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/linzero/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello linzero!'
 
+
+# @app.route('/w/')
+def hello_w():
+    return 'Hello w!'
+
+
+app.add_url_rule('/hello', view_func=hello_w)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
